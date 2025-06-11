3c59c043384478278764f53eb93fafa2334e8b86
Amir Basirati <amirbasirati13@gmail.com>
2025-06-11 21:38:20 +0330

    update

--- src/components/modules/hero/HeroSection.tsx
+++ src/components/modules/hero/HeroSection.tsx
@@ -18,68 +18,68 @@ const HeroSection = () => {
           <div className="w-5/6 md:w-1/3 lg:w-1/4 md:mr-6">
             <CarouselComponent cardsList={projectCards} />
           </div>
-          <div
-            className="pt-8 px-4 md:px-8 flex flex-col gap-y-8 "
-            style={{ direction: "rtl" }}
-          >
-            <div className="flex flex-col gap-y-8">
-              <div className="flex md:flex-row flex-col  items-center gap-x-8 gap-y-4">
-                <AvatarComponent
-                  className="w-36 h-36  p shadow-2xl border-slate-500 "
-                  source="/images/avatar.jpg"
-                  fallbackLetters="NG"
-                />
-                <h1 className="text-4xl md:text-5xl ">
-                  نرجس شیخ غفاری{" "}
-                  <span className="dark:text-fuchsia-400 text-fuchsia-600 text-6xl block mt-3">
-                    {"مهندس معمار".toUpperCase()}
-                  </span>
-                </h1>
+          <div className="pt-8 px-4 md:px-8 flex flex-col gap-y-8 ">
+            <div className="flex flex-col gap-y-10 h-full w-2/3">
+              <div className="flex flex-col   gap-x-8 gap-y-8">
+                <div className="flex gap-x-4">
+                  <AvatarComponent
+                    className="w-36 h-36  p shadow-2xl border-slate-500 "
+                    source="/images/avatar.jpg"
+                    fallbackLetters="NG"
+                  />
+                  <h1 className="text-4xl md:text-5xl ">
+                    Narjes Ghaffari{" "}
+                    <span className="dark:text-fuchsia-400 text-fuchsia-600 text-6xl block mt-3">
+                      {"the architech".toUpperCase()}
+                    </span>
+                  </h1>{" "}
+                </div>
+                <div className="flex flex-col gap-y-2">
+                  <p className=" font-light">
+                    With a creative view of form and space, architecture for me
+                    is a bridge between beauty, function and identity. In my
+                    designs, I strive to create balance between cultural
+                    origins, user needs and visual legibility. I am interested
+                    in projects that combine critical thinking, systematic
+                    approach and formal boldness.{" "}
+                  </p>
+                  <p className=""></p>
+                  <p className=""></p>
+                </div>
               </div>
 
-              <div className="flex flex-col gap-y-2">
-                <p className="">
-                  با نگاهی خلاقانه به فرم و فضا، معماری برای من پلی است میان
-                  زیبایی، عملکرد و هویت. در طراحی‌هایم تلاش می‌کنم تا میان
-                  خواستگاه فرهنگی، نیاز کاربر و خوانایی بصری، تعادل برقرار کنم.
-                  علاقه‌مند به پروژه‌هایی هستم که نگاه انتقادی، تفکر سیستماتیک و
-                  جسارت فرمی را با هم ترکیب می‌کنند.
-                </p>
-                <p className=""></p>
-                <p className=""></p>
+              <div className="flex gap-2">
+                <ButtonComponent
+                  onClicked={() => redirect("/projects")}
+                  label={
+                    <div className="flex items-center gap-2">
+                      <FaBuilding />
+                      My Projects
+                    </div>
+                  }
+                  className="md:w-fit w-full px-8 py-5 cursor-pointer bg-green-600 text-white"
+                />
+                <ButtonComponent
+                  onClicked={() => null}
+                  label={
+                    <div className="flex items-center gap-2">
+                      <FiDownload />
+                      Download CV
+                    </div>
+                  }
+                  className="md:w-fit w-full px-8 py-5 cursor-pointer bg-blue-800 text-white"
+                />
+                <ButtonComponent
+                  onClicked={() => null}
+                  label={
+                    <div className="flex items-center gap-2">
+                      <FaUser />
+                      About Me
+                    </div>
+                  }
+                  className="md:w-fit w-full px-8 py-5 cursor-pointer bg-violet-600 text-white"
+                />
               </div>
-            </div>
-            <div className="flex gap-2">
-              <ButtonComponent
-                onClicked={() => redirect("/projects")}
-                label={
-                  <div className="flex items-center gap-2">
-                    <FaBuilding />
-                    نمونه کارها
-                  </div>
-                }
-                className="md:w-fit w-full px-8 py-5 cursor-pointer bg-green-600 text-white"
-              />
-              <ButtonComponent
-                onClicked={() => null}
-                label={
-                  <div className="flex items-center gap-2">
-                    <FiDownload />
-                    دریافت رزومه
-                  </div>
-                }
-                className="md:w-fit w-full px-8 py-5 cursor-pointer bg-blue-800 text-white"
-              />
-              <ButtonComponent
-                onClicked={() => null}
-                label={
-                  <div className="flex items-center gap-2">
-                    <FaUser />
-                    درباره ی من
-                  </div>
-                }
-                className="md:w-fit w-full px-8 py-5 cursor-pointer bg-violet-600 text-white"
-              />
             </div>
           </div>
         </div>
